b929cc63bb94d169eb943dd286bf8b7cee135d8a
Igboanugwo Collins <munisco12@gmail.com>
2020-08-24 11:25:20 +0100

    Changed the port

--- backend/app.js
+++ backend/app.js
@@ -1,59 +1,57 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const multer = require('multer');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const multer = require('multer')
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require('./routes/feed')
 
-const app = express();
+const app = express()
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  }
-});
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname)
+    },
+})
 
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
 
-app.use(bodyParser.json());
-app.use(
-  multer({ storage, fileFilter }).single('image')
-);
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(bodyParser.json())
+app.use(multer({ storage, fileFilter }).single('image'))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    )
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
 
-app.use('/feed', feedRoutes);
+app.use('/feed', feedRoutes)
 
 app.use((error, req, res, next) => {
-  console.log(error);
-  const status = error.statusCode || 500;
-  const message = error.message;
-  res.status(status).json({ message: message });
-});
+    console.log(error)
+    const status = error.statusCode || 500
+    const message = error.message
+    res.status(status).json({ message: message })
+})
 
 mongoose
     .connect(
@@ -61,6 +59,7 @@ mongoose
         { useUnifiedTopology: true, useNewUrlParser: true }
     )
     .then((result) => {
-        app.listen(8080)
+        console.log('Connected!!', result)
+        app.listen(3030)
     })
     .catch((err) => console.log(err))
