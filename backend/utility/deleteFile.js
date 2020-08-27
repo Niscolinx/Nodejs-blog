0cd3cf0ad17e635eabbbffb7d3f00da144386169
Igboanugwo Collins <munisco12@gmail.com>
2020-08-28 00:18:22 +0100

    Modified the delete file function to check whether a file exists before deleting it

--- backend/utility/deleteFile.js
+++ backend/utility/deleteFile.js
@@ -1,24 +1,18 @@
 const fs = require('fs')
-const path = require('path')
 
 const deleteFile = (filePath) => {
-    console.log('the file to delete is', filePath)
-    
-    console.log('the path to delete', imgPath)
-    fs.access(filePath, fs.F_OK, (err) => {
+        fs.access(filePath, fs.F_OK, (err) => {
         if (err) {
             console.error(err)
             return
         }
-
-        console.log('the file exists')
-        //file exists
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                throw new Error('unable to delete file')
+            }
+        })
+        
     })
-    // fs.unlink(filePath, (err) => {
-    //     if (err) {
-    //         throw new Error('unable to delete file')
-    //     }
-    // })
 }
 
 exports.deleteFile = deleteFile
