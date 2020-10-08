8dfbcdc64521868055a5ea93d4493d2cca27b806
Igboanugwo Collins <munisco12@gmail.com>
2020-10-08 23:40:40 +0100

    update

--- frontend/src/components/ErrorHandler/ErrorHandler.js
+++ frontend/src/components/ErrorHandler/ErrorHandler.js
@@ -5,7 +5,7 @@ import Modal from '../Modal/Modal';
 
 const errorHandler = props => (
   <Fragment>
-    {props.error && <Backdrop onClick={props.onHandle} />}
+    {props.error && <Backdrop onClick={props.onHandlet} />}
     {props.error && (
       <Modal
         title="An Error Occurred"
